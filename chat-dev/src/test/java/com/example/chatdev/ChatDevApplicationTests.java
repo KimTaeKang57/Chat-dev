2362c8670a10b1397e27b6559d981789a4c581f8
Taekang Kim <zizoen123@gmail.com>
2022-09-16 11:03:46 +0900

    chat-dev start

---
+++ chat-dev/src/test/java/com/example/chatdev/ChatDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
